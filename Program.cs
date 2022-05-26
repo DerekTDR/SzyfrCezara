1a33184beab14a39c2d4bcfa8c908ab9f3ac16c3
DerekTDR <100943733+DerekTDR@users.noreply.github.com>
2022-05-26 22:10:07 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -3,26 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace SzyfrCezara
 {
     class Program
     {
         static void Main(string[] args)
         {
-            /* 
-             * Górne komendy są automatyczne od programu
-             * Console.WriteLine pisze tekst na całej linijce, tekst wpisuje się pod linijką
-             */
-            Console.WriteLine("Wpisz tekst do zaszyfrowania szyfrem Cezara:");
-            // foreach to pętla, która służy do odczytywania elementów kolekcji 
-                foreach (var i in Console.ReadLine())
+            char[] abc = new char[] {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+            // Alfabet "abc"
+            Console.WriteLine("Podaj zdanie do zaszyfrowania");
+            string wejscie = Console.ReadLine();
+            //Objekt "wejscie" o wartości jaka zostanie odczytana typu string
+            char[] zdn = wejscie.ToCharArray();
+            Console.Write("O ile chcesz przesunąć? ");
+            int mv;
+            // Objekt "mv" służący do przesuwania alfabetu
+            Int32.TryParse(Console.ReadLine(), out mv);
+            char[] szyfr = new char[zdn.Length];
+            //
+            // START FOR
+            for (int i = 0; i < zdn.Length; i++)
             {
-                // Konwertuje wartość na liczbę w Unicode, poprzez proste obliczenia wyznacza nam nowy alfabet
-                Console.Write(Convert.ToChar((i - 'a' + 4) + 'a'));
+                if (zdn[i] == ' ' | zdn[i] == '\n') continue;
+                // Niech sprawdza lewą i prawą stronę, \n = nowa linia
+                int pos = Array.IndexOf(abc, zdn[i]);
+                int newPos = (pos + mv) % 26;
+                // Dodawanie, a potem reszta z dzielenia
+                szyfr[i] = abc[newPos];
             }
-                // Powoduje, że program nie kończy działania od razu, nie jest to rzecz potrzebna 
+            // END FOR
+            string wyjscie = string.Join("", szyfr);
+            Console.Write(wyjscie);
+            // Wynik działań
             Console.ReadKey(true);
+            // Niech czyta klawisz, by nie kończył działania jeszcze
         }
     }
 }
